75dc6698404b30cc8305af2d13e7b8b833a578b8
Princess Itaya <taycornelius@att.net>
2020-01-30 20:29:29 -0500

    roughing out models and routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,14 @@
 // home page / index html
+app.get("/", (req, res) => {
+    res.send(index.html);
+  });
 
-
-///exercise.html /excercise
+///exercise.html 
+app.get("/exercise", (req, res) => {
+    res.send(exercise.html);
+  });
 
 //stats.html stats 
+app.get("/stats", (req, res) => {
+    res.send(stats.html);
+  });
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "notetaker";
+const collections = ["notes"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
